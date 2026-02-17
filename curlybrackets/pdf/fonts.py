ae508e91551519b2a0e8c28258f6ea5c535e5972
Margot Phoenix <margotphxw@gmail.com>
2026-02-17 01:17:04 -0800

    Add several A3 templates; Add new 8w8l templates

--- curlybrackets/pdf/fonts.py
+++ curlybrackets/pdf/fonts.py
@@ -13,15 +13,37 @@ try:
                        normal='UnGraphic', 
                        bold='UnGraphic-Bold')
 except TTFError:
-    DEFAULT_FONT = BASE_FONT
-else:
-    DEFAULT_FONT = 'UnGraphic'
+    pass
+# except TTFError:
+#     DEFAULT_FONT = BASE_FONT
+# else:
+#     DEFAULT_FONT = 'UnGraphic'
 
 try:
-    registerFont(TTFont('PretendardJP-Regular', 'PretendardJP-Regular.ttf'))
-    registerFont(TTFont('PretendardJP-Bold', 'PretendardJP-Bold.ttf'))
-    registerFontFamily('PretendardJP', 
-                       normal='PretendardJP-Regular', 
-                       bold='PretendardJP-Bold')
+    registerFont(TTFont('ZenKakuGothicAntique', 'ZenKakuGothicAntique-Regular.ttf'))
+    registerFont(TTFont('ZenKakuGothicAntique-Bold', 'ZenKakuGothicAntique-Bold.ttf'))
+    registerFontFamily('ZenKakuGothicAntique', 
+                       normal='ZenKakuGothicAntique', 
+                       bold='ZenKakuGothicAntique-Bold')
+    # registerFont(TTFont('Mplus1', 'Mplus1-Regular.ttf'))
+    # registerFont(TTFont('Mplus1-Bold', 'Mplus1-Bold.ttf'))
+    # registerFontFamily('Mplus1', 
+    #                    normal='Mplus1', 
+    #                    bold='Mplus1-Bold')
+    # registerFont(TTFont('PretendardJP-Regular', 'PretendardJP-Regular.ttf'))
+    # registerFont(TTFont('PretendardJP-Bold', 'PretendardJP-Bold.ttf'))
+    # registerFontFamily('PretendardJP', 
+    #                    normal='PretendardJP-Regular', 
+    #                    bold='PretendardJP-Bold')
+    registerFont(TTFont('NotoSansCJKtc', 'NotoSansCJKtc-VF.ttf'))
+    registerFont(TTFont('ArialUnicode', 'Arial Unicode.ttf'))
+# except TTFError:
+#     pass
 except TTFError:
-    pass
+    DEFAULT_FONT = BASE_FONT
+else:
+    # DEFAULT_FONT = 'ZenKakuGothicAntique'
+    # DEFAULT_FONT = 'Mplus1'
+    # DEFAULT_FONT = 'PretendardJP'
+    # DEFAULT_FONT = 'NotoSansCJKtc'
+    DEFAULT_FONT = 'ArialUnicode'
